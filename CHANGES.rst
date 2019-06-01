31654a39fdb5680fd86bf047481e4e93d2e47476
mriener <riener@mpia.de>
2019-06-01 19:46:32 +0200

    Preparing release 0.1

---
+++ CHANGES.rst
@@ -0,0 +1,7 @@
+GaussPy+ Changelog
+*************************
+
+0.1 (2019-06-01)
+----------------
+
+- Initial release of gausspyplus.
--- gausspyplus/tests/test_example.py
+++ gausspyplus/tests/test_example.py
@@ -2,8 +2,9 @@
 
 
 def test_primes():
-    from ..example_mod import primes
-    assert primes(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+    pass
+    # from ..example_mod import primes
+    # assert primes(10) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
 
 
 def test_deprecation():
