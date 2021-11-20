869258e300cfbfda05a29317eec5eebe4df17245
Manuel Riener <manuel.riener@icloud.com>
2021-11-20 22:23:07 +0100

    Refactor 'training' without changing functionality

--- gausspyplus/training.py
+++ gausspyplus/training.py
@@ -1,11 +1,10 @@
-# @Author: riener
-# @Date:   2018-12-19T17:26:54+01:00
-# @Filename: training.py
-# @Last modified by:   riener
-# @Last modified time: 2019-04-08T10:19:11+02:00
-
-import os
+# TODO: Fix the root cause for the following error message (caused by GaussPy?):
+#  WARNING: VisibleDeprecationWarning: Creating an ndarray from ragged nested sequences (which is a list-or-tuple of
+#  lists-or-tuples-or ndarrays with different lengths or shapes) is deprecated. If you meant to do this, you must
+#  specify 'dtype=object' when creating the ndarray.
+import functools
 import warnings
+from pathlib import Path
 
 from gausspyplus.config_file import get_values_from_config_file
 from gausspyplus.utils.output import format_warning, set_up_logger, say
@@ -31,64 +30,50 @@ class GaussPyTraining(object):
             get_values_from_config_file(
                 self, config_file, config_key='training')
 
-    def initialize(self):
-        if self.path_to_training_set is None:
-            raise Exception("Need to specify 'path_to_training_set'")
+    @functools.cached_property
+    def logger(self):
+        if not self.log_output:
+            return False
+        return set_up_logger(parentDirname=(self.gpy_dirpath if self.gpy_dirpath is not None
+                                            else Path(self.path_to_training_set).parents[1]),
+                             filename=Path(self.path_to_training_set).stem,
+                             method='g+_training')
 
-        self.dirname = os.path.dirname(self.path_to_training_set)
-        if self.gpy_dirpath is None:
-            self.gpy_dirpath = os.path.dirname(self.dirname)
-        self.file = os.path.basename(self.path_to_training_set)
-        self.filename, self.file_extension = os.path.splitext(self.file)
-
-        if self.snr_thresh is None:
-            self.snr_thresh = self.snr
-        if self.snr2_thresh is None:
-            self.snr2_thresh = self.snr
-        if self.alpha1_initial is None:
-            self.alpha1_initial = 3.
-            warnings.warn(
-                'No value for {a} supplied. Setting {a} to {b}.'.format(
-                    a='alpha1_initial', b=self.alpha1_initial))
-        if self.alpha2_initial is None:
-            self.alpha2_initial = 6.
-            warnings.warn(
-                'No value for {a} supplied. Setting {a} to {b}.'.format(
-                    a='alpha2_initial', b=self.alpha2_initial))
-
-        self.logger = False
-        if self.log_output:
-            self.logger = set_up_logger(
-                self.gpy_dirpath, self.filename, method='g+_training')
-
-    def getting_ready(self):
+    def _print_header(self):
         string = 'GaussPy training'
         banner = len(string) * '='
         heading = '\n' + banner + '\n' + string + '\n' + banner
         say(heading, verbose=self.verbose, logger=self.logger)
 
     def training(self):
-        self.initialize()
-        self.getting_ready()
-        self.gausspy_train_alpha()
+        if self.path_to_training_set is None:
+            raise Exception("Need to specify 'path_to_training_set'")
+        if self.alpha1_initial is None:
+            self.alpha1_initial = 3.
+            warnings.warn(f"No value for 'alpha1_initial' supplied. Setting {self.alpha1_initial=}.")
+        if self.two_phase_decomposition and self.alpha2_initial is None:
+            self.alpha2_initial = 6.
+            warnings.warn(f"No value for 'alpha2_initial' supplied. Setting {self.alpha2_initial=}.")
+        self._print_header()
+        self._gausspy_train_alpha()
 
-    def gausspy_train_alpha(self):
-        from .gausspy_py3 import gp as gp
+    def _gausspy_train_alpha(self):
+        from gausspyplus.gausspy_py3 import gp as gp
 
-        say(f'Using training set: {self.path_to_training_set}',
-            logger=self.logger)
+        say(f'Using training set: {self.path_to_training_set}', logger=self.logger)
 
-        g = gp.GaussianDecomposer()
+        decomposer = gp.GaussianDecomposer()
 
-        g.load_training_data(self.path_to_training_set)
-        g.set('SNR_thresh', self.snr_thresh)
-        g.set('SNR2_thresh', self.snr2_thresh)
+        decomposer.load_training_data(self.path_to_training_set)
+        decomposer.set('SNR_thresh', self.snr if self.snr_thresh is None else self.snr_thresh)
+        decomposer.set('SNR2_thresh', self.snr if self.snr2_thresh is None else self.snr2_thresh)
 
         if self.two_phase_decomposition:
-            g.set('phase', 'two')  # Set GaussPy parameters
+            decomposer.set('phase', 'two')  # Set GaussPy parameters
             # Train AGD starting with initial guess for alpha
-            g.train(alpha1_initial=self.alpha1_initial, alpha2_initial=self.alpha2_initial,
-                    logger=self.logger)
+            decomposer.train(alpha1_initial=self.alpha1_initial,
+                             alpha2_initial=self.alpha2_initial,
+                             logger=self.logger)
         else:
-            g.set('phase', 'one')
-            g.train(alpha1_initial=self.alpha1_initial, logger=self.logger)
+            decomposer.set('phase', 'one')
+            decomposer.train(alpha1_initial=self.alpha1_initial, logger=self.logger)
