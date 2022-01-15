2eaf258b5290e99e1747e1aae610a2872d192315
Manuel Riener <manuel.riener@icloud.com>
2022-01-15 14:22:52 +0100

    Refactor module 'decompose' without changing functionality

--- gausspyplus/decompose.py
+++ gausspyplus/decompose.py
@@ -1,75 +1,24 @@
-# @Author: riener
-# @Date:   2019-02-08T15:40:10+01:00
-# @Filename: decompose.py
-# @Last modified by:   riener
-# @Last modified time: 10-04-2019
-
+import functools
 import os
 import pickle
 import warnings
-
-import numpy as np
+from pathlib import Path
 
 from astropy import units as u
-from astropy.table import Table
 from astropy.wcs import WCS
-from tqdm import tqdm
 
 from gausspyplus.config_file import get_values_from_config_file
-from gausspyplus.utils.gaussian_functions import gaussian, area_of_gaussian, combined_gaussian
-from gausspyplus.utils.spectral_cube_functions import change_header, save_fits, correct_header, update_header
+from gausspyplus.utils.spectral_cube_functions import correct_header
 from gausspyplus.utils.output import set_up_logger, say
 
 
-class GaussPyDecompose(object):
-    """Decompose spectra with GaussPy+.
-
-    Attributes
-    ----------
-    path_to_pickle_file : str
-        Filepath to the pickled dictionary produced by GaussPyPrepare.
-    dirpath_gpy : str
-        Directory in which all files produced by GaussPy+ are saved
-
-    two_phase_decomposition : bool
-        'True' (default) uses two smoothing parameters (alpha1, alpha2) for the decomposition. 'False' uses only the alpha1 smoothing parameter.
-    save_initial_guesses : bool
-        Default is 'False'. Set to 'True' if initial GaussPy fitting guesses should be saved.
-    alpha1 : float
-        First smoothing parameter.
-    alpha2 : float
-        Second smoothing parameter. Only used if two_phase_decomposition is set to 'True'
-    snr_thresh : float
-        S/N threshold used for the original spectrum.
-    snr2_thresh : float
-        S/N threshold used for the second derivate of the smoothed spectrum.
-
-    use_ncpus : int
-        Number of CPUs used in the decomposition. By default 75% of all CPUs on the machine are used.
-    fitting : dct
-        Description of attribute `fitting`.
-    separation_factor : float
-        The required minimum separation between two Gaussian components (mean1, fwhm1) and (mean2, fwhm2) is determined as separation_factor * min(fwhm1, fwhm2).
-    main_beam_efficiency : float
-        Default is 'None'. Specify if intensity values should be corrected by the main beam efficiency.
-    vel_unit : astropy.units
-        Default is 'u.km/u.s'. Unit to which velocity values will be converted.
-    testing : bool
-        Default is 'False'. Set to 'True' if in testing mode.
-    verbose : bool
-        Default is 'True'. Set to 'False' if descriptive statements should not be printed in the terminal.
-    suffix : str
-        Suffix for filename of the decomposition results.
-    log_output : bool
-        Default is 'True'. Set to 'False' if terminal output should not be logged.
-
-    """
+class GaussPyDecompose:
+    """Decompose spectra with GaussPy+."""
 
     def __init__(self, path_to_pickle_file=None, config_file=''):
         self.path_to_pickle_file = path_to_pickle_file
         self.dirpath_gpy = None
 
-        # self.gausspy_decomposition = True
         self.two_phase_decomposition = True
         self.save_initial_guesses = False
         self.alpha1 = None
@@ -115,83 +64,69 @@ class GaussPyDecompose(object):
         heading = '\n' + banner + '\n' + string + '\n' + banner
         say(heading, logger=self.logger)
 
-    def initialize_data(self):
-        self.logger = False
-        if self.log_output:
-            self.logger = set_up_logger(
-                self.dirpath_gpy, self.filename, method='g+_decomposition')
+    @functools.cached_property
+    def dirpath(self):
+        # TODO: homogenize attributes self.dirpath_gpy (used here) and self.gpy_dirpath (used in training_set)
+        return self.dirpath_gpy if self.dirpath_gpy is not None else Path(self.path_to_pickle_file).parents[1]
 
-        say(f"\npickle load '{self.file}'...", logger=self.logger)
+    @functools.cached_property
+    def decomp_dirname(self):
+        (decomp_dirname := Path(self.dirpath, 'gpy_decomposed')).mkdir(parents=True, exist_ok=True)
+        return decomp_dirname
 
-        with open(self.path_to_pickle_file, "rb") as pickle_file:
-            self.pickled_data = pickle.load(pickle_file, encoding='latin1')
+    @functools.cached_property
+    def filename_in(self):
+        return Path(self.path_to_pickle_file).stem
 
-        if 'header' in self.pickled_data.keys():
-            self.header = correct_header(self.pickled_data['header'])
-            self.wcs = WCS(self.header)
-            self.velocity_increment = (
-                self.wcs.wcs.cdelt[2] * self.wcs.wcs.cunit[2]).to(
-                    self.vel_unit).value
-        if 'location' in self.pickled_data.keys():
-            self.location = self.pickled_data['location']
-        if 'nan_mask' in self.pickled_data.keys():
-            self.nan_mask = self.pickled_data['nan_mask']
-        if 'testing' in self.pickled_data.keys():
-            self.testing = self.pickled_data['testing']
-            self.use_ncpus = 1
-
-        self.data = self.pickled_data['data_list']
-        self.channels = self.pickled_data['x_values']
-        self.errors = self.pickled_data['error']
-
-    def check_settings(self):
-        if self.path_to_pickle_file is None:
-            raise Exception("Need to specify 'path_to_pickle_file'")
-
-        self.dirname = os.path.dirname(self.path_to_pickle_file)
-        self.file = os.path.basename(self.path_to_pickle_file)
-        self.filename, self.file_extension = os.path.splitext(self.file)
-
-        if self.dirpath_gpy is None:
-            self.dirpath_gpy = os.path.normpath(
-                self.dirname + os.sep + os.pardir)
-
-        self.decomp_dirname = os.path.join(self.dirpath_gpy, 'gpy_decomposed')
-        if not os.path.exists(self.decomp_dirname):
-            os.makedirs(self.decomp_dirname)
-
-        if self.main_beam_efficiency is None:
-            warnings.warn('assuming intensities are already corrected for main beam efficiency')
-
-        warnings.warn(f"converting velocity values to {self.vel_unit}")
-
-    def decompose(self):
+    @functools.cached_property
+    def logger(self):
         if self.single_prepared_spectrum:
-            self.logger = False
-            self.testing = True
-            self.use_ncpus = 1
-            self.log_output = False
-            self.getting_ready()
-            return self.start_decomposition()
-        else:
-            self.check_settings()
-            self.initialize_data()
-            self.getting_ready()
-            self.start_decomposition()
-            if 'batchdecomp_temp.pickle' in os.listdir(os.getcwd()):
-                os.remove('batchdecomp_temp.pickle')
+            return False
+        return False if not self.log_output else set_up_logger(parentDirname=self.dirpath,
+                                                               filename=self.filename_in,
+                                                               method='g+_decomposition')
 
-    def decomposition_settings(self):
-        if self.snr_negative is None:
-            self.snr_negative = self.snr
-        if self.snr_fit is None:
-            self.snr_fit = self.snr / 2.
-        if self.snr_thresh is None:
-            self.snr_thresh = self.snr
-        if self.snr2_thresh is None:
-            self.snr2_thresh = self.snr
+    @functools.cached_property
+    def pickled_data(self):
+        say(f"\npickle load '{Path(self.path_to_pickle_file).name}'...", logger=self.logger)
+        with open(self.path_to_pickle_file, "rb") as pickle_file:
+            return pickle.load(pickle_file, encoding='latin1')
 
-        self.fitting = {
+    @functools.cached_property
+    def data(self):
+        return self.pickled_data['data_list']
+
+    @functools.cached_property
+    def channels(self):
+        return self.pickled_data['x_values']
+
+    @functools.cached_property
+    def errors(self):
+        return self.pickled_data['error']
+
+    @functools.cached_property
+    def header(self):
+        return correct_header(self.pickled_data['header']) if 'header' in self.pickled_data.keys() else None
+
+    @functools.cached_property
+    def wcs(self):
+        return None if self.header is None else WCS(self.header)
+
+    @functools.cached_property
+    def velocity_increment(self):
+        return None if self.header is None else (self.wcs.wcs.cdelt[2] * self.wcs.wcs.cunit[2]).to(self.vel_unit).value
+
+    @functools.cached_property
+    def location(self):
+        return self.pickled_data['location'] if 'location' in self.pickled_data.keys() else None
+
+    @functools.cached_property
+    def nan_mask(self):
+        return self.pickled_data['nan_mask'] if 'nan_mask' in self.pickled_data.keys() else None
+
+    @functools.cached_property
+    def fitting(self):
+        return {
             'improve_fitting': self.improve_fitting,
             'min_fwhm': self.min_fwhm,
             'max_fwhm': self.max_fwhm,
@@ -208,31 +143,61 @@ class GaussPyDecompose(object):
             'separation_factor': self.separation_factor,
             'exclude_means_outside_channel_range': self.exclude_means_outside_channel_range,
             'min_pvalue': self.min_pvalue,
-            'max_ncomps': self.max_ncomps}
+            'max_ncomps': self.max_ncomps
+        }
 
-        string_gausspy = str(
-            '\ndecomposition settings:'
-            '\nGaussPy:'
-            '\nTwo phase decomposition: {a}'
-            '\nalpha1: {b}'
-            '\nalpha2: {c}'
-            '\nSNR1: {d}'
-            '\nSNR2: {e}').format(
-                a=self.two_phase_decomposition,
-                b=self.alpha1,
-                c=self.alpha2,
-                d=self.snr_thresh,
-                e=self.snr2_thresh)
+    # @functools.cached_property
+    # def testing(self):
+    #     return self.pickled_data['testing'] if 'testing' in self.pickled_data.keys() else None
+
+    def initialize_data(self):
+        if 'testing' in self.pickled_data.keys():
+            self.testing = self.pickled_data['testing']
+            if self.testing:
+                self.use_ncpus = 1
+
+    def check_settings(self):
+        if self.path_to_pickle_file is None:
+            raise Exception("Need to specify 'path_to_pickle_file'")
+
+        if self.main_beam_efficiency is None:
+            warnings.warn('assuming intensities are already corrected for main beam efficiency')
+
+        warnings.warn(f"converting velocity values to {self.vel_unit}")
+
+    def decompose(self):
+        if self.single_prepared_spectrum:
+            self.testing = True
+            self.use_ncpus = 1
+            self.getting_ready()
+            return self.start_decomposition()
+        else:
+            self.check_settings()
+            self.initialize_data()
+            self.getting_ready()
+            self.start_decomposition()
+            if 'batchdecomp_temp.pickle' in os.listdir(os.getcwd()):
+                os.remove('batchdecomp_temp.pickle')
+
+    def decomposition_settings(self):
+        self.snr_negative = self.snr if self.snr_negative is None else self.snr_negative
+        self.snr_fit = self.snr / 2 if self.snr_fit is None else self.snr_fit
+        self.snr_thresh = self.snr if self.snr_thresh is None else self.snr_thresh
+        self.snr2_thresh = self.snr if self.snr2_thresh is None else self.snr2_thresh
+
+        string_gausspy = str('\ndecomposition settings:'
+                             '\nGaussPy:'
+                             f'\nTwo phase decomposition: {self.two_phase_decomposition}'
+                             f'\nalpha1: {self.alpha1}'
+                             f'\nalpha2: {self.alpha2}'
+                             f'\nSNR1: {self.snr_thresh}'
+                             f'\nSNR2: {self.snr2_thresh}')
         say(string_gausspy, logger=self.logger)
 
-        string_gausspy_plus = ''
         if self.fitting['improve_fitting']:
-            for key, value in self.fitting.items():
-                string_gausspy_plus += str('\n{}: {}').format(key, value)
+            string_gausspy_plus = '\n' + '\n'.join([f'\n{key}: {value}' for key, value in self.fitting.items()])
         else:
-            string_gausspy_plus += str(
-                '\nimprove_fitting: {}').format(
-                    self.fitting['improve_fitting'])
+            string_gausspy_plus = f"\nimprove_fitting: {self.fitting['improve_fitting']}"
         say(string_gausspy_plus, logger=self.logger)
 
     def start_decomposition(self):
@@ -246,28 +211,28 @@ class GaussPyDecompose(object):
         self.decomposition_settings()
         say('\ndecomposing data...', logger=self.logger)
 
-        from .gausspy_py3 import gp as gp
-        g = gp.GaussianDecomposer()  # Load GaussPy
-        g.set('use_ncpus', self.use_ncpus)
-        g.set('SNR_thresh', self.snr_thresh)
-        g.set('SNR2_thresh', self.snr2_thresh)
-        g.set('improve_fitting_dict', self.fitting)
-        g.set('alpha1', self.alpha1)
+        from gausspyplus.gausspy_py3 import gp as gp
+        decomposer = gp.GaussianDecomposer()  # Load GaussPy
+        decomposer.set('use_ncpus', self.use_ncpus)
+        decomposer.set('SNR_thresh', self.snr_thresh)
+        decomposer.set('SNR2_thresh', self.snr2_thresh)
+        decomposer.set('improve_fitting_dict', self.fitting)
+        decomposer.set('alpha1', self.alpha1)
 
         if self.testing:
-            g.set('verbose', True)
-            g.set('plot', True)
+            decomposer.set('verbose', True)
+            decomposer.set('plot', True)
 
         if self.two_phase_decomposition:
-            g.set('phase', 'two')
-            g.set('alpha2', self.alpha2)
+            decomposer.set('phase', 'two')
+            decomposer.set('alpha2', self.alpha2)
         else:
-            g.set('phase', 'one')
+            decomposer.set('phase', 'one')
 
         if self.single_prepared_spectrum:
-            return g.batch_decomposition(dct=self.single_prepared_spectrum)
+            return decomposer.batch_decomposition(dct=self.single_prepared_spectrum)
 
-        self.decomposition = g.batch_decomposition(self.path_to_pickle_file)
+        self.decomposition = decomposer.batch_decomposition(self.path_to_pickle_file)
 
         self.save_final_results()
 
@@ -277,7 +242,7 @@ class GaussPyDecompose(object):
     def save_initial_guesses(self):
         say('\npickle dump GaussPy initial guesses...', logger=self.logger)
 
-        filename = f'{self.filename}{self.suffix}_fit_ini.pickle'
+        filename = f'{self.filename_in}{self.suffix}_fit_ini.pickle'
         pathname = os.path.join(self.decomp_dirname, filename)
 
         dct_initial_guesses = {}
@@ -301,20 +266,32 @@ class GaussPyDecompose(object):
         if self.two_phase_decomposition:
             dct_gausspy_settings["alpha2"] = self.alpha2
 
-        dct_final_guesses = {}
-
-        for key in ["index_fit", "best_fit_rchi2", "best_fit_aicc", "pvalue",
-                    "amplitudes_fit", "amplitudes_fit_err", "fwhms_fit",
-                    "fwhms_fit_err", "means_fit", "means_fit_err", "log_gplus",
-                    "N_neg_res_peak", "N_blended", "N_components",
-                    "quality_control"]:
-            dct_final_guesses[key] = self.decomposition[key]
+        dct_final_guesses = {
+            key: self.decomposition[key]
+            for key in [
+                "index_fit",
+                "best_fit_rchi2",
+                "best_fit_aicc",
+                "pvalue",
+                "amplitudes_fit",
+                "amplitudes_fit_err",
+                "fwhms_fit",
+                "fwhms_fit_err",
+                "means_fit",
+                "means_fit_err",
+                "log_gplus",
+                "N_neg_res_peak",
+                "N_blended",
+                "N_components",
+                "quality_control",
+            ]
+        }
 
         dct_final_guesses["gausspy_settings"] = dct_gausspy_settings
 
         dct_final_guesses["improve_fit_settings"] = self.fitting
 
-        filename = f'{self.filename}{self.suffix}_fit_fin.pickle'
+        filename = f'{self.filename_in}{self.suffix}_fit_fin.pickle'
         pathname = os.path.join(self.decomp_dirname, filename)
         pickle.dump(dct_final_guesses, open(pathname, 'wb'), protocol=2)
         say("\033[92mSAVED FILE:\033[0m '{}' in '{}'".format(
