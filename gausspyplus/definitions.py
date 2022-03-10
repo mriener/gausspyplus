f94d74d3224136b66538ff7489c798157c770b4b
Manuel Riener <manuel.riener@icloud.com>
2022-03-10 10:07:17 +0100

    Add 'PreparedSpectrum' and 'Spectrum'

--- gausspyplus/definitions.py
+++ gausspyplus/definitions.py
@@ -10,3 +10,27 @@ FitResults = namedtuple('FitResults', ["amplitude_values",
                                        "rms_noise",
                                        "reduced_chi2_value",
                                        "index"])
+
+PreparedSpectrum = namedtuple('PreparedSpectrum', ["intensity_values",
+                                                   "position_yx",
+                                                   "rms_noise",
+                                                   "signal_intervals",
+                                                   "noise_spike_intervals",
+                                                   "index"])
+
+fields = [
+    "index",
+    "intensity_values",
+    "position_yx",
+    "rms_noise",
+    "signal_intervals",
+    "noise_spike_intervals",
+    "n_fit_components",
+    "amplitude_values",
+    "mean_values",
+    "fwhm_values",
+    "reduced_chi2_value",
+]
+defaults = [None for field in fields]
+
+Spectrum = namedtuple('Spectrum', fields, defaults=defaults)
